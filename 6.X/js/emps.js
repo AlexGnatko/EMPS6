3e61478da318fe853bfa14b2da11f8a29c4f93cd
Alex Gnatko <agnatko@gmail.com>
2021-02-05 20:38:10 +0800

    New EMPS.navigate

--- 6.X/js/emps.js
+++ 6.X/js/emps.js
@@ -314,12 +314,15 @@ var EMPS = {
                 });
         });
     },
-    guid() {
+    guid: function() {
         function _p8(s) {
             var p = (Math.random().toString(16)+"000000000").substr(2,8);
             return s ? "-" + p.substr(0,4) + "-" + p.substr(4,4) : p ;
         }
         return _p8() + _p8(true) + _p8(true) + _p8();
+    },
+    navigate: function(href) {
+        window.location = href;
     }
 };
 
