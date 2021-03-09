499ed7410fbde53ca3488e1997caff663dbd1216
Alex Gnatko <agnatko@gmail.com>
2021-03-09 12:02:39 +0800

    Downloads

--- 6.5/common/smarty.plugins.php
+++ 6.5/common/smarty.plugins.php
@@ -1,6 +1,41 @@
 <?php
 global $emps;
 
+function smarty_plugin_downloads($params)
+{
+    global $smarty, $emps, $up;
+
+    require_once($emps->common_module('uploads/uploads.class.php'));
+
+    if (!isset($sp_up)) {
+        $sp_up = new EMPS_Uploads;
+    }
+
+    $context_id = $params['context'];
+    if ($context_id) {
+        $lst = $sp_up->list_files($context_id, 1000);
+        $smarty->assign("filelist", $lst);
+        return $smarty->fetch("db:page/filelist");
+    }
+
+    $list = $params['list'];
+    if ($list) {
+        $lst = array();
+        $xx = explode(",", $list);
+        foreach ($xx as $v) {
+            $id = $v + 0;
+            $ra = $emps->db->get_row("e_files", "id = " . $id);
+            $ra['fsize'] = format_size($ra['size']);
+            $lst[] = $ra;
+
+        }
+
+        $smarty->assign("filelist", $lst);
+        return $smarty->fetch("db:page/filelist");
+    }
+}
+
+
 $fn = $emps->common_module('config/smarty/plugins.php');
 if (file_exists($fn)) {
     require_once $fn;
