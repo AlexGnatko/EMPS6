3a1e49d81e0e4aaa24ec1a968618a02dc297046d
Alex Gnatko <agnatko@gmail.com>
2021-03-12 20:35:52 +0800

    The install script

--- install/install.sh
+++ install/install.sh
@@ -13,6 +13,7 @@ apt -y install mc
 
 apt -y install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
 echo "deb http://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list
+pushd
 cd /root
 wget http://www.webmin.com/jcameron-key.asc
 apt-key add jcameron-key.asc
@@ -20,6 +21,7 @@ apt update
 apt -y install apt-transport-https
 apt -y install webmin
 apt -y install libio-socket-inet6-perl
+popd
 
 ### Install smartmontools
 
@@ -58,4 +60,4 @@ apt -y install python3-certbot-nginx
 
 ### Run the PHP installer script
 
-/usr/bin/php ./install.php
+php ./install.php
