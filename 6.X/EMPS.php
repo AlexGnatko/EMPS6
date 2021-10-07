50ec13c26d8e4c4510b7b2a10a14de5f07ccf5b8
Alex Gnatko <agnatko@gmail.com>
2021-10-08 00:27:35 +0800

    php_session_id()

--- 6.X/EMPS.php
+++ 6.X/EMPS.php
@@ -338,14 +338,5 @@ class EMPS_Common
         header("Cache-Control: max-age=" . $seconds);
     }
 
-    public function php_session_id() {
-        global $emps;
 
-        $code = $_COOKIE['PHPSESSID'];
-        $row = $emps->db->get_row("e_php_sessions", "sess_id = '{$code}'");
-        if ($row) {
-            return $row['id'];
-        }
-        return 0;
-    }
 }
