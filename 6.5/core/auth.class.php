6bfa215f88cb73d747e01c88063f31c62e0ed544
Alex Gnatko <agnatko@gmail.com>
2022-12-27 20:39:27 +0800

    Update to newer PHP

--- 6.5/core/auth.class.php
+++ 6.5/core/auth.class.php
@@ -1037,7 +1037,7 @@ class EMPS_Auth
         $pwd = "";
         for ($i = 0; $i < $cnt; $i++) {
             $ic = mt_rand(0, $len);
-            $c = $line{$ic};
+            $c = substr($line, $ic, 1);
             $pwd .= $c;
         }
         return $pwd;
