6977751224d3e6ea1a0196874ad5b6cd9f824cfb
Alex <agnatko@gmail.com>
2025-10-06 11:28:47 +0800

    Sortable fix

--- 6.X/js/vue/sortable/vue-sortable.js
+++ 6.X/js/vue/sortable/vue-sortable.js
@@ -1,59 +1,59 @@
 emps_scripts.push(function() {
-    EMPS.vue3_scripts.push(function() {
-        if (EMPS.vue_version() === 2) {
-            var vSortable = {}
-            var Sortable = typeof require === 'function'
-                ? require('sortablejs')
-                : window.Sortable
+    if (EMPS.vue_version() === 2) {
+        var vSortable = {}
+        var Sortable = typeof require === 'function'
+            ? require('sortablejs')
+            : window.Sortable
 
-            if (!Sortable) {
-                throw new Error('[vue-sortable] cannot locate Sortable.js.')
-            }
-
-            // exposed global options
-            vSortable.config = {}
-
-            vSortable.install = function (Vue) {
-                Vue.directive('sortable',
-                    {
-                        inserted: function (el) {
-//                    alert("inserted: " + JSON.stringify(el.options));
-                            var sortable = new Sortable(el, el.options)
-                            if (this.arg && !this.vm.sortable) {
-                                this.vm.sortable = {}
-                            }
-// Throw an error if the given ID is not unique
-                            if (this.arg && this.vm.sortable[this.arg]) {
-                                console.warn('[vue-sortable] cannot set already defined sortable id: \'' + this.arg + '\'')
-                            }
-                            else if (this.arg) {
-                                this.vm.sortable[this.arg] = sortable
-                            }
-                        },
-                        bind: function (el, binding) {
-                            var options = Object.assign({}, window.emps_sortable_options);
-                            options = Object.assign(options, binding.value || {});
-                            el.options = options;
-                        }
-                    })
-            }
-
-            if (typeof exports == "object") {
-                module.exports = vSortable
-            } else if (typeof define == "function" && define.amd) {
-                define([], function () {
-                    return vSortable
-                })
-            } else if (window.Vue) {
-                window.vSortable = vSortable
-                let app = Vue;
-                if (EMPS.vue_version() == 3) {
-                    app = EMPS.mainapp;
-                }
-                app.use(vSortable)
-            }
+        if (!Sortable) {
+            throw new Error('[vue-sortable] cannot locate Sortable.js.')
         }
 
+        // exposed global options
+        vSortable.config = {}
+
+        vSortable.install = function (Vue) {
+            Vue.directive('sortable',
+                {
+                    inserted: function (el) {
+//                    alert("inserted: " + JSON.stringify(el.options));
+                        var sortable = new Sortable(el, el.options)
+                        if (this.arg && !this.vm.sortable) {
+                            this.vm.sortable = {}
+                        }
+// Throw an error if the given ID is not unique
+                        if (this.arg && this.vm.sortable[this.arg]) {
+                            console.warn('[vue-sortable] cannot set already defined sortable id: \'' + this.arg + '\'')
+                        }
+                        else if (this.arg) {
+                            this.vm.sortable[this.arg] = sortable
+                        }
+                    },
+                    bind: function (el, binding) {
+                        var options = Object.assign({}, window.emps_sortable_options);
+                        options = Object.assign(options, binding.value || {});
+                        el.options = options;
+                    }
+                })
+        }
+
+        if (typeof exports == "object") {
+            module.exports = vSortable
+        } else if (typeof define == "function" && define.amd) {
+            define([], function () {
+                return vSortable
+            })
+        } else if (window.Vue) {
+            window.vSortable = vSortable
+            let app = Vue;
+            if (EMPS.vue_version() == 3) {
+                app = EMPS.mainapp;
+            }
+            app.use(vSortable)
+        }
+    }
+
+    EMPS.vue3_scripts.push(function() {
         function isVue3() {
             try {
                 return typeof EMPS !== 'undefined' && EMPS && typeof EMPS.vue_version === 'function'
